ca29e1a7145c414073007478f8307b68e0878a92
Wesley Van Koeckhoven (VL) <Wesley.VanKoeckhoven_Vistalink@outlook.com>
2020-02-27 10:41:33 +0100

    Add project files.

---
+++ MyNugetAssembly/TestFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vistalink.NuGetTest
+{
+    /// <summary>
+    /// Some test functions for our NuGet Package
+    /// </summary>
+    public static class TestFunctions
+    {
+        /// <summary>
+        /// Returns some text
+        /// </summary>
+        public static string SomeText => "I Like Nuggets";
+
+        /// <summary>
+        /// Prints the <see cref="SomeText"/> property
+        /// </summary>
+        public static void PrintSomeText() => Console.WriteLine(SomeText);
+    }
+}
---
+++ NuGetTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vistalink.NuGetTest;
+
+namespace NuGetTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"Printing the value: '{TestFunctions.SomeText}'");
+
+            Console.WriteLine("Now calling the function...");
+            TestFunctions.PrintSomeText();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+
+            Console.ReadLine();
+
+        }
+    }
+}
